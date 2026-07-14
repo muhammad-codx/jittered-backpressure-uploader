0a1f651a96ea6b5dc86758694dbacfc41b9a6544
Muhammad <boymirzayev06@icloud.com>
2026-07-14 16:53:04 +0500

    m

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,8 @@ REST_FRAMEWORK = {
 
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'WebCraft API',
-    'DESCRIPTION': 'WebCraft IT Training Center backend API',
+    'TITLE': 'PI',
+    'DESCRIPTION': 'backend API',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     'COMPONENT_SPLIT_REQUEST': True,
